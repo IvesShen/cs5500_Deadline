05886aa5f5f2680ca9897da185d492bfa416fa24
messfish <messfish@users.noreply.github.com>
2017-10-11 14:20:49 -0400

    finish the update method

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask
-app = Flask(__name__)
+from rest import rest
 
-@app.route('/')
-def hello_world():
-   return 'Hello, World!'
+app = Flask(__name__)
+app.register_blueprint(rest)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
