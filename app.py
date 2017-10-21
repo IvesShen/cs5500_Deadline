f8555ab35c100dfab392ea12c0dbbf9e78584f02
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-20 18:38:00 -0700

    simple model working properly

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import flask
+from flask import Flask
 from flask import request, jsonify, session
-import sqlalchemy
-from sqlalchemy import inspect, desc, create_engine
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
 import json
 # import models
 # from models import engine, dbsession
@@ -12,27 +12,35 @@ __credits__ = ["Priyank Chaudhary"]
 __version__ = "0.1"
 __status__ = "Production"
 
-app = flask.Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app = Flask(__name__)
+app.config.from_object(environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 #TODO: move this to a configuration file.
 config = {
-    'auth': False #enable authentication.
+    'auth': False  #enable authentication.
 }
+
 
 def is_login_valid():
     return session.get('auth_email') != None
+
 
 @app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 def index():
     return "Welcome to Pet Project!"
 
+
 @app.route("/_info")
 def info():
-    return jsonify({"Application": "flask-based-pet-project %s" % __version__,
-                    "Powered By": "flask %s, sqlalchemy %s" % (flask.__version__, sqlalchemy.__version__),
-                    })
+    return jsonify({
+        "Application":
+        "flask-based-pet-project %s" % __version__,
+        "Powered By":
+        "flask %s, sqlalchemy %s" % (flask.__version__,
+                                     sqlalchemy.__version__),
+    })
+
 
 @app.route("/pet", methods=["POST"])
 def add_pet():
@@ -40,7 +48,7 @@ def add_pet():
     :returns: TODO
 
     """
-    pass
+
 
 @app.route("/pet/<int:pet_id>", methods=["GET"])
 def find_pet_id(pet_id):
@@ -52,6 +60,7 @@ def find_pet_id(pet_id):
     """
     pass
 
+
 @app.route("/pet/all/", methods=["GET"])
 def find_all_known_pets():
     """TODO: Docstring for find_all_known_pets.
@@ -60,32 +69,28 @@ def find_all_known_pets():
     """
     pass
 
-@app.route()
+    # @app.route()
 
-@app.route("/pet/<int:pet_id>", methods=["PUT"])
-def update_pet_info(pet_id):
-    """TODO: Docstring for .
+    # @app.route("/pet/<int:pet_id>", methods=["PUT"])
+    # def update_pet_info(pet_id):
+    #     """TODO: Docstring for .
 
-    :pet_id: TODO
-    :returns: TODO
+    #     :pet_id: TODO
+    #     :returns: TODO
 
-    """
+    #     """
     pass
 
 
-@app.route("/pet/<int:pet_id>/history", methods=["POST"])
+# @app.route("/pet/<int:pet_id>/history", methods=["POST"])
 
+# @app.route("/pet", methods=["POST"])
 
-@app.route("/pet", methods=["POST"])
+# @app.route("/pet", methods=["POST"])
 
+# @app.route("/pet", methods=["POST"])
 
-@app.route("/pet", methods=["POST"])
-
-
-@app.route("/pet", methods=["POST"])
-
-if __name__ == "__main__":
-    print("pet project v%s" % __version__)
-    app.secret_key = "some secret"
-    app.debug = True
-    app.run()
+print("pet project v%s" % __version__)
+app.secret_key = "some secret"
+app.debug = True
+app.run()
