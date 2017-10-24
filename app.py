f7eee4fbfe240028a9f90a265db8ce8d7a4426a7
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-24 09:07:23 -0700

    well tested GETs

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from database import db_session, init_db
 from flask import Flask
 from flask import request, jsonify, session
 from os import environ
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, inspect
+from models import Pet
 import json
 # import models
 # from models import engine, dbsession
@@ -12,18 +14,24 @@ __credits__ = ["Priyank Chaudhary"]
 __version__ = "0.1"
 __status__ = "Production"
 
+init_db()
 app = Flask(__name__)
 app.config.from_object(environ['APP_SETTINGS'])
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-#TODO: move this to a configuration file.
+# TODO: move this to a configuration file.
 config = {
-    'auth': False  #enable authentication.
+    'auth': False  # enable authentication.
 }
 
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
 def is_login_valid():
-    return session.get('auth_email') != None
+    return session.get('auth_email') is not None
 
 
 @app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
@@ -44,17 +52,25 @@ def add_pet():
     :returns: TODO
 
     """
+    data = request.get_json(force=True)
+    print(data)
+    to_write = Pet(**data)
+    db_session.add(to_write)
+    db_session.commit()
+    return jsonify({"pet.id": to_write.id})
 
 
 @app.route("/pet/<int:pet_id>", methods=["GET"])
-def find_pet_id(pet_id):
+def find_pet_by_id(pet_id):
     """TODO: Docstring for .
 
     :pet_id: TODO
     :returns: TODO
 
     """
-    pass
+    query_res = Pet.query.filter(Pet.id == pet_id).first()
+    data = object_as_dict(query_res)
+    return jsonify({"status": "success", "data": data})
 
 
 @app.route("/pet/all/", methods=["GET"])
@@ -63,19 +79,108 @@ def find_all_known_pets():
     :returns: TODO
 
     """
-    pass
+    query_res = Pet.query.all()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
 
-    # @app.route()
 
-    # @app.route("/pet/<int:pet_id>", methods=["PUT"])
-    # def update_pet_info(pet_id):
-    #     """TODO: Docstring for .
+@app.route("/pet/<int:pet_id>/<string:place>", methods=["PUT"])
+def move_pet_to_place(pet_id, place):
+    """TODO: Docstring for .
 
-    #     :pet_id: TODO
-    #     :returns: TODO
+    :place: TODO
+    :pet_id: TODO
+    :returns: TODO
 
-    #     """
-    pass
+    """
+    query_res = Pet.query.filter(Pet.id == pet_id).first()
+    data = object_as_dict(query_res)
+    return jsonify({"status": "success", "data": data})
+
+
+@app.route("/pet/<string:name>", methods=["GET"])
+def find_pets_by_name(name):
+    """TODO: Docstring for .
+
+    :name: TODO
+    :returns: TODO
+
+    """
+    query_res = Pet.query.filter(Pet._name == name).all()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
+
+
+@app.route("/pet/<string:kind>", methods=["GET"])
+def find_pets_by_kind(kind):
+    """TODO: Docstring for .
+
+    :kind: TODO
+    :returns: TODO
+
+    """
+    query_res = Pet.query.filter(Pet._kind == kind).all()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
+
+
+@app.route("/pet/<string:place>", methods=["GET"])
+def find_pets_by_place(place):
+    """TODO: Docstring for .
+
+    :place: TODO
+    :returns: TODO
+
+    """
+    query_res = Pet.query.filter(Pet._place == place).all()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
+
+
+@app.route("/pet/<string:color>", methods=["GET"])
+def find_pets_by_color(color):
+    """TODO: Docstring for .
+
+    :color: TODO
+    :returns: TODO
+
+    """
+    query_res = Pet.query.filter(Pet._color == color).all()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
+
+
+@app.route("/pet/<string:breed>", methods=["GET"])
+def find_pets_by_breed(breed):
+    """TODO: Docstring for .
+
+    :breed: TODO
+    :returns: TODO
+
+    """
+    query_res = Pet.query.filter(Pet._breed == breed).all()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
+
+
+@app.route("/pet/<string:gender>", methods=["GET"])
+def find_pets_by_gender(gender):
+    """TODO: Docstring for .
+
+    :gender: TODO
+    :returns: TODO
+
+    """
+    query_res = Pet.query.filter(Pet._gender == gender).first()
+    data = [object_as_dict(r) for r in query_res]
+    return jsonify({"status": "success", "data": data})
+
+
+def object_as_dict(obj):
+    return {
+        c.key: getattr(obj, c.key)
+        for c in inspect(obj).mapper.column_attrs
+    }
 
 
 # @app.route("/pet/<int:pet_id>/history", methods=["POST"])
