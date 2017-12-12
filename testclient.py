86fea8d07f7bc3e50fc8b4a2289d5f91979c442e
messfish <messfish@users.noreply.github.com>
2017-12-12 10:49:02 -0800

    reformat the code to reduce the redundency

--- testclient.py
+++ testclient.py
@@ -18,278 +18,83 @@ if not os.path.exists(output_path):
 # Based on the request of the other team, I need to use this cookie to 
 # access their API. Note you need to login first and then get the
 # JSESSIONID from the cookies, without that you cannot generate json outputs! 
-cookies = dict(JSESSIONID='PASTE THE ID HERE!')   
+cookies = dict(JSESSIONID='PASTE THE ID HERE!') 
 
-# This part is for the pet-controller
-url_for_all_pets = 'https://petrescueorganization.herokuapp.com/pets'
-r = requests.get(url_for_all_pets, cookies = cookies)
-all_pets_output_path = os.path.join(output_path, 'get_all_pets.txt')
-write_file = open(all_pets_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+# this is the part for testing the GET method.
+def test_get_method(url_for_test, output_file):
+    r = requests.get(url_for_test, cookies = cookies)
+    output_file_path = os.path.join(output_path, output_file)
+    write_file = open(output_file_path, 'w')
+    write_file.write(r.text)
+    write_file.close()
 
-url_for_single_pet = 'https://petrescueorganization.herokuapp.com/pets/64'
-r = requests.get(url_for_single_pet, cookies = cookies)
-single_pet_output_path = os.path.join(output_path, 'get_single_pet.txt')
-write_file = open(single_pet_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+test_get_method('https://petrescueorganization.herokuapp.com/pets', 'get_all_pets.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/pets/64', 'get_single_pet.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/pets/features', 'get_pet_features.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/pet-types', 'get_all_pet_types.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/pet-types/44', 'get_single_pet_type.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/shelters', 'get_all_shelters.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/shelters/43', 'get_single_shelter.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/tracks', 'get_all_tracks.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/tracks/1', 'get_single_track.txt')
+test_get_method('https://petrescueorganization.herokuapp.com/tracks/pets', 'get_track_pets.txt')
 
-url_for_inserting_pet = 'https://petrescueorganization.herokuapp.com/pets'
-inserted_pet_path = os.path.join(input_path, 'insert_new_pet.txt')
-read_file = open(inserted_pet_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_pet, json = data, cookies = cookies)
-insert_pet_output_path = os.path.join(output_path, 'insert_single_pet.txt')
-write_file = open(insert_pet_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+# this is the part for testing the POST method.
+def test_post_method(url_for_test, input_file, output_file):
+    input_file_path = os.path.join(input_path, input_file)
+    read_file = open(input_file_path, 'r')
+    data = json.loads(read_file.read())
+    read_file.close()
+    r = requests.post(url_for_test, json = data, cookies = cookies)
+    output_file_path = os.path.join(output_path, output_file)
+    write_file = open(output_file_path, 'w')
+    write_file.write(r.text)
+    write_file.close()
 
-# simply perform the insertion with the same pet id again to see what will happen
-# if we insert pets with duplicated ids.
-url_for_inserting_pet = 'https://petrescueorganization.herokuapp.com/pets'
-inserted_pet_path = os.path.join(input_path, 'insert_new_pet.txt')
-read_file = open(inserted_pet_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_pet, json = data, cookies = cookies)
-insert_pet_output_path = os.path.join(output_path, 'insert_single_pet_with_duplicated_id.txt')
-write_file = open(insert_pet_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+# note since the post is for insertion, after doing the insertion. I will do the same
+# insertion again to see what will happen if I insert duplicated ids.
+# also for deletion's sake, I set all the ID as 213 to let delete operate on that.
+test_post_method('https://petrescueorganization.herokuapp.com/pets', 'insert_new_pet.txt', 'insert_single_pet.txt')
+test_post_method('https://petrescueorganization.herokuapp.com/pets', 'insert_new_pet.txt', 'insert_single_pet_with_duplicate_id.txt')
+test_post_method('https://petrescueorganization.herokuapp.com/pet-types', 'insert_new_pet_type.txt', 'insert_single_pet_type.txt')
+test_post_method('https://petrescueorganization.herokuapp.com/pet-types', 'insert_new_pet_type.txt', 'insert_single_pet_type_with_duplicate_id.txt')
+test_post_method('https://petrescueorganization.herokuapp.com/shelters', 'insert_new_shelter.txt', 'insert_single_shelter.txt')   
+test_post_method('https://petrescueorganization.herokuapp.com/shelters', 'insert_new_shelter.txt', 'insert_single_shelter_with_duplicate_id.txt')
+test_post_method('https://petrescueorganization.herokuapp.com/tracks', 'insert_new_track.txt', 'insert_single_track.txt')
+test_post_method('https://petrescueorganization.herokuapp.com/tracks', 'insert_new_track.txt', 'insert_single_track_with_duplicate_id.txt')
 
-url_for_pet_features = 'https://petrescueorganization.herokuapp.com/pets/features'
-r = requests.get(url_for_pet_features, cookies = cookies)
-pet_features_output_path = os.path.join(output_path, 'get_pet_features.txt')
-write_file = open(pet_features_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+# this is the part for testing the PUT method.
+def test_put_method(url_for_test, input_file, output_file):
+    input_file_path = os.path.join(input_path, input_file)
+    read_file = open(input_file_path, 'r')
+    data = json.loads(read_file.read())
+    read_file.close()
+    r = requests.put(url_for_test, json = data, cookies = cookies)
+    output_file_path = os.path.join(output_path, output_file)
+    write_file = open(output_file_path, 'w')
+    write_file.write(r.text)
+    write_file.close()
 
-url_for_updating_pet = 'https://petrescueorganization.herokuapp.com/pets'
-updated_pet_path = os.path.join(input_path, 'update_pet.txt')
-read_file = open(updated_pet_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.put(url_for_updating_pet, json = data, cookies = cookies)
-updating_pet_output_path = os.path.join(output_path, 'updating_pet.txt')
-write_file = open(updating_pet_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+test_put_method('https://petrescueorganization.herokuapp.com/pets', 'update_pet.txt', 'updating_pet.txt')
+test_put_method('https://petrescueorganization.herokuapp.com/pet-types', 'update_pet_type.txt', 'updating_pet_type.txt')
+test_put_method('https://petrescueorganization.herokuapp.com/shelters', 'update_shelter.txt', 'updating_shelter.txt')
+test_put_method('https://petrescueorganization.herokuapp.com/tracks', 'update_track.txt', 'updating_track.txt')
 
-url_for_delete_pet = 'https://petrescueorganization.herokuapp.com/pets/213'
-r = requests.delete(url_for_delete_pet, cookies = cookies)
-delete_pet_output_path = os.path.join(output_path, 'delete_pet.txt')
-write_file = open(delete_pet_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+# this is the part for testing the DELETE method.
+def test_delete_method(url_for_test, output_file):
+    r = requests.delete(url_for_test, cookies = cookies)
+    output_file_path = os.path.join(output_path, output_file)
+    write_file = open(output_file_path, 'w')
+    write_file.write(r.text)
+    write_file.close()
 
-# do the deletion with the same pet id again and check what will happen.
-url_for_delete_pet = 'https://petrescueorganization.herokuapp.com/pets/213'
-r = requests.delete(url_for_delete_pet, cookies = cookies)
-delete_pet_output_path = os.path.join(output_path, 'delete_pet_with_same_id.txt')
-write_file = open(delete_pet_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# this part is for the pet-type-controller
-url_for_all_pet_types = 'https://petrescueorganization.herokuapp.com/pet-types'
-r = requests.get(url_for_all_pet_types, cookies = cookies)
-all_pet_types_output_path = os.path.join(output_path, 'get_all_pet_types.txt')
-write_file = open(all_pet_types_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_single_pet_type = 'https://petrescueorganization.herokuapp.com/pet-types/44'
-r = requests.get(url_for_single_pet_type, cookies = cookies)
-single_pet_type_output_path = os.path.join(output_path, 'get_single_pet_type.txt')
-write_file = open(single_pet_type_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_inserting_pet_type = 'https://petrescueorganization.herokuapp.com/pet-types'
-inserted_pet_type_path = os.path.join(input_path, 'insert_new_pet_type.txt')
-read_file = open(inserted_pet_type_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_pet_type, json = data, cookies = cookies)
-insert_pet_type_output_path = os.path.join(output_path, 'insert_single_pet_type.txt')
-write_file = open(insert_pet_type_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# simply perform the insertion with the same pet type again to see what will happen
-# if we insert pet type with duplicated ids.
-url_for_inserting_pet_type = 'https://petrescueorganization.herokuapp.com/pet-types'
-inserted_pet_type_path = os.path.join(input_path, 'insert_new_pet_type.txt')
-read_file = open(inserted_pet_type_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_pet_type, json = data, cookies = cookies)
-insert_pet_type_output_path = os.path.join(output_path, 'insert_single_pet_type_with_duplicated_id.txt')
-write_file = open(insert_pet_type_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_updating_pet_type = 'https://petrescueorganization.herokuapp.com/pet-types'
-updated_pet_type_path = os.path.join(input_path, 'update_pet_type.txt')
-read_file = open(updated_pet_type_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.put(url_for_updating_pet_type, json = data, cookies = cookies)
-updating_pet_type_output_path = os.path.join(output_path, 'updating_pet_type.txt')
-write_file = open(updating_pet_type_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_delete_pet_type = 'https://petrescueorganization.herokuapp.com/pet-types/213'
-r = requests.delete(url_for_delete_pet_type, cookies = cookies)
-delete_pet_type_output_path = os.path.join(output_path, 'delete_pet_type.txt')
-write_file = open(delete_pet_type_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# do the deletion with the same pet type again and check what will happen.
-url_for_delete_pet_type = 'https://petrescueorganization.herokuapp.com/pet-types/213'
-r = requests.delete(url_for_delete_pet_type, cookies = cookies)
-delete_pet_type_output_path = os.path.join(output_path, 'delete_pet_type_with_same_id.txt')
-write_file = open(delete_pet_type_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# this part is for the shelter controller
-url_for_all_shelters = 'https://petrescueorganization.herokuapp.com/shelters'
-r = requests.get(url_for_all_shelters, cookies = cookies)
-all_shelters_output_path = os.path.join(output_path, 'get_all_shelters.txt')
-write_file = open(all_shelters_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_single_shelter = 'https://petrescueorganization.herokuapp.com/shelters/43'
-r = requests.get(url_for_single_shelter, cookies = cookies)
-single_shelter_output_path = os.path.join(output_path, 'get_single_shelter.txt')
-write_file = open(single_shelter_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_inserting_shelter = 'https://petrescueorganization.herokuapp.com/shelters'
-inserted_shelter_path = os.path.join(input_path, 'insert_new_shelter.txt')
-read_file = open(inserted_shelter_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_shelter, json = data, cookies = cookies)
-insert_shelter_output_path = os.path.join(output_path, 'insert_single_shelter.txt')
-write_file = open(insert_shelter_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# simply perform the insertion with the same pet type again to see what will happen
-# if we insert shelter with duplicated ids.
-url_for_inserting_shelter = 'https://petrescueorganization.herokuapp.com/shelters'
-inserted_shelter_path = os.path.join(input_path, 'insert_new_shelter.txt')
-read_file = open(inserted_shelter_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_shelter, json = data, cookies = cookies)
-insert_shelter_output_path = os.path.join(output_path, 'insert_single_shelter_with_duplicated_id.txt')
-write_file = open(insert_shelter_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_updating_shelter = 'https://petrescueorganization.herokuapp.com/shelters'
-updated_shelter_path = os.path.join(input_path, 'update_shelter.txt')
-read_file = open(updated_shelter_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.put(url_for_updating_shelter, json = data, cookies = cookies)
-updating_shelter_output_path = os.path.join(output_path, 'updating_shelter.txt')
-write_file = open(updating_shelter_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_delete_shelter = 'https://petrescueorganization.herokuapp.com/shelters/213'
-r = requests.delete(url_for_delete_shelter, cookies = cookies)
-delete_shelter_output_path = os.path.join(output_path, 'delete_shelter.txt')
-write_file = open(delete_shelter_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# do the deletion with the same shelter again and check what will happen.
-url_for_delete_shelter = 'https://petrescueorganization.herokuapp.com/shelters/213'
-r = requests.delete(url_for_delete_shelter, cookies = cookies)
-delete_shelter_output_path = os.path.join(output_path, 'delete_shelter_with_same_id.txt')
-write_file = open(delete_shelter_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# this part is for the track controller
-url_for_all_tracks = 'https://petrescueorganization.herokuapp.com/tracks'
-r = requests.get(url_for_all_tracks, cookies = cookies)
-all_tracks_output_path = os.path.join(output_path, 'get_all_tracks.txt')
-write_file = open(all_tracks_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_single_track = 'https://petrescueorganization.herokuapp.com/tracks/1'
-r = requests.get(url_for_single_track, cookies = cookies)
-single_track_output_path = os.path.join(output_path, 'get_single_track.txt')
-write_file = open(single_track_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_inserting_track = 'https://petrescueorganization.herokuapp.com/tracks'
-inserted_track_path = os.path.join(input_path, 'insert_new_track.txt')
-read_file = open(inserted_track_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_track, json = data, cookies = cookies)
-insert_track_output_path = os.path.join(output_path, 'insert_single_track.txt')
-write_file = open(insert_track_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# simply perform the insertion with the same pet type again to see what will happen
-# if we insert pet type with duplicated ids.
-url_for_inserting_track = 'https://petrescueorganization.herokuapp.com/tracks'
-inserted_track_path = os.path.join(input_path, 'insert_new_track.txt')
-read_file = open(inserted_track_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.post(url_for_inserting_track, json = data, cookies = cookies)
-insert_track_output_path = os.path.join(output_path, 'insert_single_track_with_duplicated_id.txt')
-write_file = open(insert_track_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_updating_track = 'https://petrescueorganization.herokuapp.com/tracks'
-updated_track_path = os.path.join(input_path, 'update_track.txt')
-read_file = open(updated_track_path, 'r')
-data = json.loads(read_file.read())
-read_file.close()
-r = requests.put(url_for_updating_track, json = data, cookies = cookies)
-updating_track_output_path = os.path.join(output_path, 'updating_track.txt')
-write_file = open(updating_track_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_delete_track = 'https://petrescueorganization.herokuapp.com/tracks/213'
-r = requests.delete(url_for_delete_track, cookies = cookies)
-delete_track_output_path = os.path.join(output_path, 'delete_shelter.txt')
-write_file = open(delete_track_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-# do the deletion with the same pet type again and check what will happen.
-url_for_delete_track = 'https://petrescueorganization.herokuapp.com/tracks/213'
-r = requests.delete(url_for_delete_track, cookies = cookies)
-delete_track_output_path = os.path.join(output_path, 'delete_shelter_with_same_id.txt')
-write_file = open(delete_track_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
-
-url_for_get_track_pets = 'https://petrescueorganization.herokuapp.com/tracks/pets'
-r = requests.get(url_for_get_track_pets, cookies = cookies)
-get_track_pets_output_path = os.path.join(output_path, 'get_track_pets.txt')
-write_file = open(get_track_pets_output_path, 'w')
-write_file.write(r.text)
-write_file.close()
+# note I will do the same deletion to see what will happen if I delete something
+# that is not appeared in the website database.
+test_delete_method('https://petrescueorganization.herokuapp.com/pets/213', 'delete_pet.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/pets/213', 'delete_pet_with_same_id.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/pet-types/213', 'delete_pet_type.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/pet-types/213', 'delete_pet_type_with_same_id.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/shelters/213', 'delete_shelter.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/shelters/213', 'delete_shelter_with_same_id.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/tracks/213', 'delete_track.txt')
+test_delete_method('https://petrescueorganization.herokuapp.com/tracks/213', 'delete_track_with_same_id.txt')
